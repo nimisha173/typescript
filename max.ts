8cdb8a859cc8690aee959008ee41ca0c907dd29b
nimisha173 <nimishars32@gmail.com>
2023-03-30 15:55:22 +0530

    30/03/2023

---
+++ max.ts
@@ -0,0 +1,12 @@
+function findMax(numbers) {
+    var max = numbers[0];
+    for (var i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+var numbers = [5, 3, 8, 1, 9, 4];
+var maxNumber = findMax(numbers); // returns 9
+console.log(maxNumber); // output: 9
